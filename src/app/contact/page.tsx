6f9dd0da6bcf08ac4afbeda9e5a925e29c035088
Aldi Abdu Malik <aldi_abdu.konsultan@work.bri.co.id>
2024-07-09 17:03:39 +0700

    fix: add contact us page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,7 +9,7 @@ export default function page() {
         <h2>Email</h2>
         <p>Send us an email at <a href="mailto:ptkadetech@gmail.com">ptkadetech@gmail.com</a>, and we will get back to you as soon as possible.</p>
         <h2>Whatsapp</h2>
-        <p>Chat us at <a href="wa.me/6289622142528"><i>+(62) 896 2214 2528</i></a> during our business hours: Monday to Friday, 8 AM - 5 PM.</p>
+        <p>Chat us at <a href="https://wa.me/6289622142528"><i>+(62) 896 2214 2528</i></a> during our business hours: Monday to Friday, 8 AM - 5 PM.</p>
       </div>
     </MainLayout>
   )
